5b8b930e0b7a8b04db65b1c736a44fc8711eeb3f
Mia Winther Jørgensen <miawintherjorgensen@Mias-MacBook-Air.local>
2017-02-23 12:38:47 +0100

    load json produktliste

--- petrograd.js
+++ petrograd.js
@@ -2,10 +2,21 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
 	console.log("siden vises");
+
+	//Læs data ind fra fil/JSON
+	$.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
 	visProdukt();
 }
 
-function visProdukt() {
+function visProduktListe(listen) {
+	console.table(listen);
+	listen.forEach(visProdukt);
+
+}
+
+function visProdukt(produkt) {
+	console.log(produkt);
 	// klon produkt template
 	var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 	//indsæt data i klon
