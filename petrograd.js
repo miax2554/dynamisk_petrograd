f5ce44af1bd91f7ac8dd2707a608bfc4b9adcff1
Mia Winther Jørgensen <miawintherjorgensen@Mias-MacBook-Air.local>
2017-02-23 13:02:00 +0100

    tilføjet billeder og rabatpris

--- petrograd.js
+++ petrograd.js
@@ -5,8 +5,6 @@ function sidenVises() {
 
 	//Læs data ind fra fil/JSON
 	$.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
-
-	visProdukt();
 }
 
 function visProduktListe(listen) {
@@ -20,6 +18,14 @@ function visProdukt(produkt) {
 	// klon produkt template
 	var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 	//indsæt data i klon
+	klon.querySelector(".data_navn").innerHTML = produkt.navn;
+	klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+	var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
+	klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
+
+	klon.querySelector(".data_billede").src = "/imgs/medium/" + produkt.billede + "-md.jpg";
+
 	//appen klon til produkt_liste
 	document.querySelector(".produktliste").appendChild(klon);
 }
